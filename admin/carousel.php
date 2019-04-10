c5f367e16b024ea21d312ed31fc45a0d550bd852
Nischal Shrestha <nischal@github.com>
2019-04-10 13:09:41 +0545

    Solution plus site 10-4-2019

---
+++ admin/carousel.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<title>Edit Carousel</title>
+</head>
+<body>
+<form action="upload.php" method="post" enctype="multipart/form-data">
+    Select Image File to Upload:
+    <input type="file" name="file">
+    <input type="submit" name="submit" value="Upload">
+</form>
+
+</body>
+</html>
